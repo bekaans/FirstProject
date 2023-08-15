04bae167db3d5604b4c0dd4fea7b2eec8526026b
Berke Kaan <bekaans@icloud.com>
2023-08-15 13:29:45 +0300

    Unistall Entity Framework 6 nuget and Add [Key]Category and Product

--- EntityLayer/Entities/Product.cs
+++ EntityLayer/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EntityLayer.Entities
 {
     public class Product
     {
+        [Key]
         public int ProductID { get; set; }
         public String ProductName { get; set; }
         public int Stock { get; set; }
