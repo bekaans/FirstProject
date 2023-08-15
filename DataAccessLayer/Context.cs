69b39ec1f7c1a716973b8b24453ff36a45ae4ed0
Berke Kaan <bekaans@icloud.com>
2023-08-15 15:52:34 +0300

    Add reference DataAccessLayer From Product

---
+++ DataAccessLayer/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        public DbSet<Product> MyProperty { get; set; }
+
+
+    }
+}
